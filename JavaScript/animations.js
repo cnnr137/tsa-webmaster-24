4f3c8d356e328bb1dae3b5dd775427dcedd99de0
Euphoriyy <96319862+Euphoriyy@users.noreply.github.com>
2024-05-23 00:43:13 +0000

    chore: format

--- JavaScript/animations.js
+++ JavaScript/animations.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-function reveal_image() {
-    var reveals = document.querySelectorAll(".reveal-image");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 100;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
+document.addEventListener("DOMContentLoaded", () => {
+	function reveal_image() {
+		var reveals = document.querySelectorAll(".reveal-image")
+		for (var i = 0; i < reveals.length; i++) {
+			var windowHeight = window.innerHeight
+			var elementTop = reveals[i].getBoundingClientRect().top
+			var elementVisible = 100
+			if (elementTop < windowHeight - elementVisible) {
+				reveals[i].classList.add("active")
+			} else {
+				reveals[i].classList.remove("active")
+			}
+		}
+	}
 
-  window.addEventListener("scroll", reveal_image);
+	window.addEventListener("scroll", reveal_image)
 
-reveal();
-});
+	reveal()
+})
